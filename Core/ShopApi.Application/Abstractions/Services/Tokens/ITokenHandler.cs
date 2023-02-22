d56b57d32493f5aa026c1d47104364318ef48697
Ilkin Rufullayev <rufullayevilkin66@gmail.com>
2023-02-22 10:40:38 +0400

    authentication feature

---
+++ Core/ShopApi.Application/Abstractions/Services/Tokens/ITokenHandler.cs
@@ -0,0 +1,15 @@
+﻿using ShopApi.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ShopApi.Application.DTOs.AuthDTO;
+
+namespace ShopApi.Application.Abstractions.Services.Tokens
+{
+    public interface ITokenHandler
+    {
+        string CreateToken(AppUser user, string role);
+    }
+}
---
+++ Infrastructure/ShopApi.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShopApi.Application.Abstractions.Services.Tokens;
+using ShopApi.Infrastructure.Services.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApi.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenHandler, TokenHandler>();
+        }
+    }
+}
